42ff5fa541f9974b240ca6fa2637934d4af130ed
evcorona <eldav.corona@gmail.com>
2023-09-03 00:49:22 -0600

    💄 Homepage layout with vehicles section displayed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-import clsx from 'clsx';
+import Input from 'components/Input';
+import VehiclesTable from 'components/VehiclesTable';
 
 export default function Home() {
   return (
-    <main
-      className={clsx(
-        'mt-14 p-2',
-        'flex flex-col',
-        'items-center justify-between',
-        'h-screen'
-      )}
-    >
-      <h1>Technical test</h1>
-    </main>
+    <div className='space-y-4 h-full'>
+      <section className='bg-slate-500 w-full h-1/2'>Map</section>
+      <section className='w-full h-full space-y-4'>
+        <h1 className='text-base font-semibold leading-6 text-gray-900'>
+          Vehículos
+        </h1>
+        <Input type='search' placeholder='Buscar vehículo...' />
+        <VehiclesTable />
+      </section>
+    </div>
   );
 }
