edc932b20ddec05db72819975a2bf3c591d40fda
evcorona <eldav.corona@gmail.com>
2023-09-04 09:14:40 -0600

    🐛 Fix file conflict

--- src/app/components/Map/Map.tsx
+++ src/app/components/Map/Map.tsx
@@ -1,15 +1,9 @@
 import MapController from 'components/Map/MapController'
 import { MapRoute } from 'types'
 import getMapRoutes from 'api/getMapRoutes'
-import mapRoutesJsonMock from 'assets/mapRoutesMock.json'
 
-interface Props {
-  activeMock?: boolean
-}
-export default async function Map(props: Props) {
-  const mapRoutes: MapRoute[] = props.activeMock
-    ? mapRoutesJsonMock
-    : await getMapRoutes()
+export default async function Map() {
+  const mapRoutes: MapRoute[] = await getMapRoutes()
 
   return <MapController mapRoutes={mapRoutes} />
 }
